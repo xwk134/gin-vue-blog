4fd7dd4bfcb43d5aa91b921a5cf44fff3596a892
2148583859@qq.com <3437061763>
2023-05-09 21:44:08 +0800

    广告管理_删除广告

--- gvb_server/routers/advert_router.go
+++ gvb_server/routers/advert_router.go
@@ -7,4 +7,6 @@ func (router RouterGroup) AdvertRouter() {
 	router.POST("adverts", app.AdvertCreateView)
 	router.GET("adverts", app.AdvertListView)
 	router.PUT("adverts/:id", app.AdvertUpdateView)
+	router.DELETE("adverts", app.AdvertRemoveView)
+
 }
